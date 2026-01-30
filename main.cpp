d1077c5c80137c2abfc71ef17ceee09f908274fe
cyantere <vuholosu@gmail.com>
2026-01-30 16:47:14 +0800

    shading 练习

--- main.cpp
+++ main.cpp
@@ -6,40 +6,30 @@ extern std::vector<double> zbuffer;     // the depth buffer
 
 struct PhongShader : IShader {
     const Model &model;
-    vec4 l;              // light direction in eye coordinates
-    vec2  varying_uv[3]; // triangle uv coordinates, written by the vertex shader, read by the fragment shader
-    vec4 varying_nrm[3]; // normal per vertex to be interpolated by the fragment shader
-    vec4 tri[3];         // triangle in view coordinates
+    vec3 l;          // light direction in eye coordinates
+    vec3 tri[3];     // triangle in eye coordinates
 
     PhongShader(const vec3 light, const Model &m) : model(m) {
-        l = normalized((ModelView*vec4{light.x, light.y, light.z, 0.})); // transform the light vector to view coordinates
+        l = normalized((ModelView*vec4{light.x, light.y, light.z, 0.}).xyz()); // transform the light vector to view coordinates
     }
 
     virtual vec4 vertex(const int face, const int vert) {
-        varying_uv[vert]  = model.uv(face, vert);
-        varying_nrm[vert] = ModelView.invert_transpose() * model.normal(face, vert);
-        vec4 gl_Position = ModelView * model.vert(face, vert);
-        tri[vert] = gl_Position;
+        vec3 v = model.vert(face, vert).xyz();                   // current vertex in object coordinates
+        vec4 gl_Position = ModelView * vec4{v.x, v.y, v.z, 1.};
+        tri[vert] = gl_Position.xyz();                            // in eye coordinates
         return Perspective * gl_Position;                         // in clip coordinates
     }
 
     virtual std::pair<bool,TGAColor> fragment(const vec3 bar) const {
-        mat<2,4> E = { tri[1]-tri[0], tri[2]-tri[0] };
-        mat<2,2> U = { varying_uv[1]-varying_uv[0], varying_uv[2]-varying_uv[0] };
-        mat<2,4> T = U.invert() * E;
-        mat<4,4> D = {normalized(T[0]),  // tangent vector
-                      normalized(T[1]),  // bitangent vector
-                      normalized(varying_nrm[0]*bar[0] + varying_nrm[1]*bar[1] + varying_nrm[2]*bar[2]), // interpolated normal
-                      {0,0,0,1}}; // Darboux frame
-        vec2 uv = varying_uv[0] * bar[0] + varying_uv[1] * bar[1] + varying_uv[2] * bar[2];
-        vec4 n = normalized(D.transpose() * model.normal(uv));
-        vec4 r = normalized(n * (n * l)*2 - l);                   // reflected light direction
-        double ambient  = .4;                                     // ambient light intensity
-        double diffuse  = 1.*std::max(0., n * l);                 // diffuse light intensity
-        double specular = (.5+2.*sample2D(model.specular(), uv)[0]/255.) * std::pow(std::max(r.z, 0.), 35);  // specular intensity, note that the camera lies on the z-axis (in eye coordinates), therefore simple r.z, since (0,0,1)*(r.x, r.y, r.z) = r.z
-        TGAColor gl_FragColor = sample2D(model.diffuse(), uv);
-        for (int channel : {0,1,2})
-            gl_FragColor[channel] = std::min<int>(255, gl_FragColor[channel]*(ambient + diffuse + specular));
+        TGAColor gl_FragColor = {255, 255, 255, 255};             // output color of the fragment
+        vec3 n = normalized(cross(tri[1]-tri[0], tri[2]-tri[0])); // triangle normal in eye coordinates
+        vec3 r = normalized(n * (n * l)*2 - l);                   // reflected light direction
+        double ambient = .3;                                      // ambient light intensity
+        double diff = std::max(0., n * l);                        // diffuse light intensity
+        double spec = std::pow(std::max(r.z, 0.), 35);            // specular intensity, note that the camera lies on the z-axis (in eye coordinates), therefore simple r.z, since (0,0,1)*(r.x, r.y, r.z) = r.z
+        for (int channel : {0,1,2}) {                             // for each color channel
+            gl_FragColor[channel] *= std::min(1., ambient + .4*diff + .9*spec);
+        }
         return {false, gl_FragColor};                             // do not discard the pixel
     }
 };
@@ -61,7 +51,7 @@ int main(int argc, char** argv) {
     init_perspective(norm(eye-center));                        // build the Perspective matrix
     init_viewport(width/16, height/16, width*7/8, height*7/8); // build the Viewport    matrix
     init_zbuffer(width, height);
-    TGAImage framebuffer(width, height, TGAImage::RGB, {177, 195, 209, 255});
+    TGAImage framebuffer(width, height, TGAImage::RGB);
 
     for (int m=1; m<argc; m++) {                    // iterate through all input objects
         Model model(argv[m]);                       // load the data
@@ -77,4 +67,3 @@ int main(int argc, char** argv) {
     framebuffer.write_tga_file("framebuffer.tga");
     return 0;
 }
-
